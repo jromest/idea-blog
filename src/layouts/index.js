fb264174273a0a6d73a1862a843409ab8dda6e48
Jrom <jromestiller@gmail.com>
2018-06-24 10:36:20 +0800

    Move styles to global and layout.css

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Header from '../components/header'
-import './index.css'
+import './global.css'
+import './layout.css'
 
 const Layout = ({ children, data }) => (
   <div>
@@ -15,14 +16,7 @@ const Layout = ({ children, data }) => (
       ]}
     />
     <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div className='app-wrapper'>
       {children()}
     </div>
   </div>
