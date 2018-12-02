e3769e73c21e4cb0235878b65eb0593a7f86c524
jromest <jromestiller@gmail.com>
2018-12-03 06:03:58 +0800

    chore: opt out css modules in header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,10 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Social from './Social'
 
-import style from '../styles/components/header.module.scss'
-
 const Header = props => (
-  <header className={style.container}>
-    <div className={style.wrapper}>
-      <Link to="/" className={style.title}>
+  <header className="header">
+    <div className="container container-main header-wrapper">
+      <Link to="/" className="title">
         {props.title}
       </Link>
       <Social {...props} />
