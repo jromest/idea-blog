023ff56b85106cf389336e1e37ff79495c6d91d1
jromest <jromestiller@gmail.com>
2019-01-20 08:06:23 +0800

    feat: minor tweaks for the footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = props => (
   <footer className="footer">
     <div className="container container-main footer-wrapper flex-row">
       <Social color="#666" />
-      <div>&copy; 2018 {props.title}</div>
+      <div className="footer-copyright">&copy; 2018 {props.title}</div>
     </div>
   </footer>
 )
