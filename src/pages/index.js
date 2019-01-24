229a131b2216906d876af055e2bc21dd5e324b8b
jromest <jromestiller@gmail.com>
2019-01-25 07:40:26 +0800

    feat: initial content sections on the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,43 @@ const IndexPage = ({ data }) => {
         isHomepage
       />
       <div className="container container-main section">
-        <h2 className="section-title">Hello World</h2>
+        <h2 className="section-title">About</h2>
+        <ul>
+          <li>Develops</li>
+          <li>Designs</li>
+          <li>Community Builder</li>
+        </ul>
+      </div>
+      <div className="container container-main section">
+        <h2 className="section-title">Writings</h2>
+        <h4>Coming soon...</h4>
+      </div>
+      <div className="container container-main section">
+        <h2 className="section-title">Projects and Works</h2>
+        <ul>
+          <li>Agroworks</li>
+          <li>filipino-bad-words-lists</li>
+          <li>The old creative website</li>
+        </ul>
+        <p>View more on my Github...</p>
+      </div>
+      <div className="container container-main section">
+        <h2 className="section-title">Technologies I've Work With</h2>
+        <ul>
+          <li>Javascript</li>
+          <li>NodeJS</li>
+          <li>ReactJS</li>
+          <li>SASS</li>
+          <li>CSS 3</li>
+          <li>HTML 5</li>
+        </ul>
+      </div>
+      <div className="container container-main section">
+        <h2 className="section-title">Stay Connected</h2>
+        <ul>
+          <li>Email</li>
+          <li>Twitter</li>
+        </ul>
       </div>
       <div className="bio-section">
         <Bio />
