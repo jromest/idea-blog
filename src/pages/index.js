29837ddaf1593b3cfc4cadc29546bc5fa810512e
jromest <jromestiller@gmail.com>
2019-01-25 20:36:33 +0800

    feat: add contents in about section

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,10 +17,22 @@ const IndexPage = ({ data }) => {
       />
       <div className="container container-main section">
         <h2 className="section-title">About</h2>
+        <p>
+          I'm a front end web developer based in Manila, Philippines. I design
+          and develop experiences that make people's lives simple. I specialize
+          in JavaScript and ReactJS 'n' friends that I learned mostly
+          self-taught and growing my skills and learning new things every day.
+        </p>
+        <p>
+          Outside of work, I am probably running, trekking or hiking. Otherwise,
+          probably watch movies or reading books.
+        </p>
         <ul>
           <li>Develops</li>
           <li>Designs</li>
           <li>Community Builder</li>
+          <li>UI Design</li>
+          <li>Responsive Design</li>
         </ul>
       </div>
       <div className="container container-main section">
