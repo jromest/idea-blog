45cfdb1b4d742d2759be96b21080d142cffbe0e6
jromest <jromestiller@gmail.com>
2019-02-04 16:21:28 +0800

    feat: about section design

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,9 @@ import Hero from '../components/Hero'
 import Bio from '../components/Bio'
 import Tech from '../components/Tech'
 
+import webDesignImage from '../assets/responsive-design.svg'
+import checkIcon from '../assets/checkmark-outline.svg'
+
 const IndexPage = ({ data }) => {
   const { siteMetadata } = data.site
   const { author } = siteMetadata
@@ -17,24 +20,45 @@ const IndexPage = ({ data }) => {
         isHomepage
       />
       <div className="container container-main section">
-        <h2 className="section-title">About</h2>
+        <div className="flex-row section-about">
+          <div className="flex-col section-about-container">
+            <img
+              className="section-about-container--image"
+              src={webDesignImage}
+              alt="responsive web design"
+            />
+          </div>
+          <div className="flex-col section-about-container">
+            <ul className="about-list">
+              {[
+                'Web Developer',
+                'Web Designer',
+                'Community Leader',
+                'Life-long Learner',
+              ].map((item, index) => (
+                <li key={index} className="about-list-item">
+                  <img
+                    className="about-list-image"
+                    src={checkIcon}
+                    alt="check"
+                  />
+                  <span>{item}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
         <p>
           I'm a front end web developer based in Manila, Philippines. I design
           and develop experiences that make people's lives simple. I specialize
-          in JavaScript and ReactJS 'n' friends that I learned mostly
-          self-taught and growing my skills and learning new things every day.
+          in <strong>JavaScript</strong> and <strong>React</strong> 'n' friends
+          that I learned mostly self-taught and growing my skills and learning
+          new things every day.
         </p>
         <p>
           Outside of work, I am probably running, trekking or hiking. Otherwise,
           probably watch movies or reading books.
         </p>
-        <ul>
-          <li>Develops</li>
-          <li>Designs</li>
-          <li>Community Builder</li>
-          <li>UI Design</li>
-          <li>Responsive Design</li>
-        </ul>
       </div>
       <div className="container container-main section">
         <h2 className="section-title">Writings</h2>
