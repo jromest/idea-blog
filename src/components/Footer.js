62021044b00c90746788ddf31f9a86062cc76ed3
jromest <jromestiller@gmail.com>
2019-02-20 23:53:54 +0800

    feat: use semantic tag in footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = props => (
   <footer className="footer">
     <div className="container container-main footer-wrapper flex-row">
       <Social color="#666" />
-      <div className="footer-copyright">&copy; 2019 {props.name}</div>
+      <small className="footer-copyright">&copy; 2019 {props.name}</small>
     </div>
   </footer>
 )
